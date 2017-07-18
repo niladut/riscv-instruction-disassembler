870abd9b80217b623d4665feecd740e82b561726
Niladri Dutta <niladridutta31@gmail.com>
2017-07-18 11:23:38 +0530

    Working Disassembler

--- disassembler.cc
+++ disassembler.cc
@@ -13,22 +13,33 @@ int main()
 
 	int xlen=32;
 	insn_bits_t instruction;
-	disassembler_t disassembler(xlen);
-	// disassembler_t * disassembler = new disassembler_t(xlen);
-    scanf("%lux",&instruction);
+	// disassembler_t disassembler(xlen);
+	
+    disassembler_t * disassembler = new disassembler_t(xlen);
+    
+    while(scanf("%lx",&instruction))
+    {
+        // switch((char)instruction)
+        // {
+        //     case 'q':
+        //         delete(disassembler);
+        //         return 0;
+        //         break;
+        //     case 'h':
+        //         fprintf(stdout, "Command Help\n\n");
+        //         fprintf(stdout, "h\t:: Display this Command Help.\n");
+        //         fprintf(stdout, "q\t:: Quit.\n");
+        //         break;
+        // }
 
-    instruction = ((instruction>>24)&0xff) | // move byte 3 to byte 0
-                    ((instruction<<8)&0xff0000) | // move byte 1 to byte 2
-                    ((instruction>>8)&0xff00) | // move byte 2 to byte 1
-                    ((instruction<<24)&0xff000000); // byte 0 to byte 3
+    	insn_t insn(instruction);
 
-	insn_t insn(instruction);
+    	std::string str = disassembler->disassemble(insn);
 
-	// std::string str = disassembler.disassemble(insn);
-
-    fprintf(stderr, "%s\n", disassembler.disassemble(insn).c_str());
-    // fprintf(stderr, "%s\n", str.c_str());
-
-    // delete(disassembler);
+        // fprintf(stderr, "%s\n", disassembler.disassemble(insn).c_str());
+        fprintf(stdout, "%lx\t:: %s\n", instruction,str.c_str());
+        // fprintf(stderr, "%lx\t:: %s\n", instruction,str.c_str());
+    }
+    delete(disassembler);
     return 0;
 }
